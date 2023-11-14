353ca020bb59f1c14d718cacf1517aae08ce188e
KuroNeko <1419229777@qq.com>
2023-11-14 17:49:24 +0800

    update: 跳蚤格式调整

--- src/main/java/com/neko/txbot/plugin/TarKovMarketPlugin.java
+++ src/main/java/com/neko/txbot/plugin/TarKovMarketPlugin.java
@@ -33,13 +33,15 @@ public class TarKovMarketPlugin extends BotPlugin {
                 tarKovMarketVos.forEach(it -> {
                     MsgUtils msg = MsgUtils.builder();
                     msg.text("名称：").text(it.getCnName() + "\n");
-                    msg.text("占格：").text(it.getSize() + "\n");
-                    msg.text("基础价格：").text(it.getBasePrice() + it.getTraderPriceCur() + "\n");
+                    msg.text("波动 24h：").text(it.getChange24() + "%").text("  7d：").text(it.getChange7d() + "%\n");
+                    msg.text("基础价格：").text(it.getBasePrice() + "₽" + "\n");
                     msg.text(it.getTraderName()).text("：").text(it.getTraderPrice() + it.getTraderPriceCur() + "\n");
                     if (it.isCanSellOnFlea()) {
-                        msg.text("跳蚤日价：").text(it.getAvgDayPrice() + it.getTraderPriceCur() + "\n");
-                        msg.text("跳蚤周价：").text(it.getAvgWeekPrice() + it.getTraderPriceCur());
+                        msg.text("跳蚤日价：").text(it.getAvgDayPrice() + "₽" + "\n");
+                        msg.text("跳蚤周价：").text(it.getAvgWeekPrice() + "₽" + "\n");
+                        msg.text("单格：").text(it.getAvgDayPrice() / it.getSize() + "₽");
                     } else {
+                        msg.text("单格：").text(it.getTraderPrice() / it.getSize() + it.getTraderPriceCur() + "\n");
                         msg.text("跳蚤禁售!");
                     }
 //                    bot.sendChannelMsg(event.getChannelId(), event.getId(), msg.build(), it.getEnImg());
