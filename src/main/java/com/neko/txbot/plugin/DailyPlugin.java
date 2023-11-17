e358bfaf44eb46e928b73ac559e82998ecbcdc76
KuroNeko <1419229777@qq.com>
2023-11-17 10:06:19 +0800

    update: 日报功能

--- src/main/java/com/neko/txbot/plugin/DailyPlugin.java
+++ src/main/java/com/neko/txbot/plugin/DailyPlugin.java
@@ -40,7 +40,7 @@ public class DailyPlugin extends BotPlugin {
     public int onChannelMessage(Bot bot, ChannelMessageEvent event) {
         if (event.getNoAtContent().startsWith(CMD)) {
             MsgUtils msg = getCmdMsg();
-            bot.sendChannelMsg(event.getChannelId(), event.getId(), "", msg.getUrl());
+            bot.sendChannelMsg(event.getChannelId(), event.getId(), msg.build(), msg.getUrl());
             return MESSAGE_BLOCK;
         } else if (event.getNoAtContent().startsWith(CMD2)) {
             MsgUtils msg = getCmd2Msg();
