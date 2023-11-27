89de132b6df00f0a8fe9186464558c4cca4455f4
KuroNeko <1419229777@qq.com>
2023-11-27 14:49:20 +0800

    update: 自建代理

--- src/main/java/com/neko/txbot/core/msg/ImgMsg.java
+++ src/main/java/com/neko/txbot/core/msg/ImgMsg.java
@@ -2,7 +2,8 @@ package com.neko.txbot.core.msg;
 
 public class ImgMsg extends MediaMsg {
     private boolean proxy = false;
-    public static final String PROXY_IMG_URL = "https://i3.wp.com/";
+//    public static final String PROXY_IMG_URL = "https://i3.wp.com/";
+    public static final String PROXY_IMG_URL = "https://ct-game.com/rp/file/";
 
     public ImgMsg img(String url) {
         this.url = url;
